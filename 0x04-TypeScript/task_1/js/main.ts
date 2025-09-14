275042942d4ff1f016a7f9d79768738b86bd46c4
Gelagay G. <gela.sirta3@gmail.com>
2025-09-14 23:46:36 +0300

    Add initial project files for TypeScript Teacher Interface project

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -9,6 +9,11 @@ interface Teacher {
   [key: string]: any; // allows additional attributes
 }
 
+// Directors interface extends Teacher
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
 // Example usage
 const teacher3: Teacher = {
   firstName: "John",
@@ -19,3 +24,13 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+const director1: Directors = {
+  firstName: "John",
+  lastName: "Doe",
+  location: "London",
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
